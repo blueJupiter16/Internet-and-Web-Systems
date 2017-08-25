c771558dc0d47540934fc7051f54b099326bdca6
Junaid <junaid.tinwala.96@gmail.com>
2017-08-25 22:02:25 +0530

    overlay effect

---
+++ craiglist/script.js
@@ -0,0 +1,9 @@
+function switchOffOverlay(){
+   document.getElementById("overlay").style.display = "none";
+   document.getElementsByClassName("list")[0].style.display = "none";
+}
+
+function switchOnOverlay(){
+   document.getElementById("overlay").style.display = "block";
+   document.getElementsByClassName("list")[0].style.display = "inline-block";
+}
