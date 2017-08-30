e701a8a7aad6dc53323692ff35042bb1733ef8cc
Junaid <junaid.tinwala.96@gmail.com>
2017-08-30 16:40:34 +0530

    Select Region Functionality

--- craiglist/script.js
+++ craiglist/script.js
@@ -8,3 +8,17 @@ function switchOnOverlay(){
    document.getElementById("overlay").style.display = "block";
    document.getElementsByClassName("list")[0].style.display = "inline-block";
 }
+
+function parseURL(){
+
+
+   //console.log(window.location.href.split("#")[1]);
+   //console.log(window.location.href);
+   var s = document.getElementsByClassName("dropbtn")[0].innerHTML;
+   document.getElementsByClassName("dropbtn")[0].innerHTML = window.location.href.split("#")[1] + s;
+   //alert(document.getElementsByClassName("dropbtn")[0].innerHTML);
+}
+
+function mapPage(){
+   window.location.replace("file:///C:/SNU/Sem-7/Internet and Web Systems/Lab/craiglist/index.html");
+}
